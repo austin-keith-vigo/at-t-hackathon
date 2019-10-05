1300a1f74d079f0de954915ef7b44b7051319405
Austin Vigo <avigo@uci.edu>
2019-10-05 14:00:45 -0700

    works to animate a title in
    works to animate a title in

--- maps/src/screens/Loginscreen.js
+++ maps/src/screens/Loginscreen.js
@@ -1,29 +1,46 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Dimensions} from 'react-native';
 import GLOBALS from './../Globals';
 import {connect} from 'react-redux';
 
 //Components
 import {SliderTile} from './../components';
 
+const _renderTitle = (sliderAnimationRunning) => {
+  if(!sliderAnimationRunning){
+    return <Text style={styles.titleStyle}>MAPS</Text>
+  };
+};
+
 const Loginscreen = (props) => {
   return (
     <View style={styles.viewStyle}>
-      {console.log(props)}
       <SliderTile
-        text="MAPS"
         animationDuration={2000}
       />
+      {_renderTitle(props.sliderAnimationRunning)}
+
     </View>
   );
 };
+
+const screenHeight = Math.round(Dimensions.get('window').height);
+const screenWidth = Math.round(Dimensions.get('window').width);
 
 const styles = {
   viewStyle: {
     flex: 1,
     backgroundColor: GLOBALS.COLORS.PURPLE
+  },
+  titleStyle: {
+    fontSize: 55,
+    color: 'white',
+    position: 'absolute',
+    top: screenHeight*.32,
+    left: screenWidth*.6
   }
 };
+
 const mapStateToProps = state => {
   return {
       sliderAnimationRunning: state.login.sliderAnimationRunning
