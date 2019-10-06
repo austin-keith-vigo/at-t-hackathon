0326318854ef72b9226af9c074321f064ee6e428
Austin Vigo <avigo@uci.edu>
2019-10-05 23:09:01 -0700

    direction screen is done
    done

--- maps/src/components/DirectionsView.js
+++ maps/src/components/DirectionsView.js
@@ -9,25 +9,26 @@ import GLOBALS from './../Globals';
 import {connect} from 'react-redux';
 import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
 
-const _renderItem = (direction) => {
+const _renderItem = (direction,index) => {
   return (
-    <View style={{height: '20%', width: '30%'}}>
-      <Text>{direction}</Text>
+    <View style={styles.listViewStyle}>
+      <View style={styles.number}>
+        <Text style={styles.numberTextStyle}>{index}</Text>
+      </View>
+      <Text style={styles.listTextStyle}>{direction}</Text>
     </View>
   );
 };
 const DirectionsView = (props) => {
   return(
-    <GestureRecognizer onSwipeDown = {props.closeSubView}>
       <View style={styles.viewStyle}>
         <FlatList
           style={{flex:1}}
           data={props.directionsData}
-          renderItem={({item,index})=>_renderItem(item)}
+          renderItem={({item,index})=>_renderItem(item, index)}
           keyExtractor={(index)=>index.toString()}
         />
       </View>
-    </GestureRecognizer>
   );
 };
 
@@ -37,6 +38,35 @@ const styles = {
     height: GLOBALS.SCREEN_HEIGHT*.9,
     backgroundColor: GLOBALS.COLORS.DARK_PURPLE,
     borderRadius: 20
+  },
+  listViewStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    borderBottomWidth: 2,
+    borderBottomColor: 'black',
+    height: 80
+  },
+  listTextStyle: {
+    fontSize: 20,
+    fontColor: 'white',
+    flex: 1,
+    flexWrap: 'wrap',
+    marginTop: 10,
+    marginLeft: 5,
+    marginRight: 5
+  },
+  number: {
+    height: 60,
+    width: 60,
+    backgroundColor: GLOBALS.COLORS.PURPLE,
+    borderRadius: 30,
+    marginRight: 5,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  numberTextStyle: {
+    fontSize: 20,
   }
 }
 
